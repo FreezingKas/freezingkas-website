4f0e9205dbb1d14e9eb03b3a7ea4c0993bd7b04c
FreezingKas <57356272+FreezingKas@users.noreply.github.com>
2022-10-02 13:38:24 +0200

    petite update de la page CTF Team

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,8 @@
 import Terminal, { ColorMode, LineType } from '../src/components/terminal/index.tsx';
 import { useState } from 'react';
+import {Link} from 'react-router-dom'; 
+
 import './App.css'
-import {Link} from 'react-router-dom';
 
 
 function App() {
@@ -14,44 +15,62 @@ function App() {
     function onInput(string) {
         let ld = [...terminalLineData];
         ld.push({type: LineType.Input, value: string});
-        if(string.toLocaleLowerCase() === 'help') {
-            ld.push({type: LineType.Output, value: 'Available commands: '});
-            ld.push({type: LineType.Output, value: 'help : Displays this help message'});
-            ld.push({type: LineType.Output, value: 'whoami : Displays who is FreezingKas'});
-            ld.push({type: LineType.Output, value: 'github : Displays my GitHub profile'});
-            ld.push({type: LineType.Output, value: 'write-ups : Open my write-ups page'});
-            ld.push({type: LineType.Output, value: 'projects : Open my projects page'});
-            ld.push({type: LineType.Output, value: 'rootme : Open my root-me profile'});
-            ld.push({type: LineType.Output, value: 'hackthebox : Open my HackTheBox profile'});
-            ld.push({type: LineType.Output, value: 'tryhackme : Open my TryHackMe profile'})
-            ld.push({type: LineType.Output, value: 'clear : Clears the terminal'});
-        } else if(string.toLocaleLowerCase() === 'whoami') {
-            ld.push({type: LineType.Output, value: 'FreezingKas : Software Cyber Security student at ENSIBS (Vannes)'});
-        } else if(string.toLocaleLowerCase() === 'github') {
-            ld.push({type: LineType.Output, value: 'Opening Github profile...'});
-            window.open('https://github.com/FreezingKas', '_blank');
-        } else if(string.toLocaleLowerCase() === 'write-ups') {
-            ld.push({type: LineType.Output, value: 'Opening Write-Ups page...'});
-            window.open(process.env.URL + 'write-ups/', '_blank');
-        } else if (string.toLocaleLowerCase() === 'projects') {
-            ld.push({type: LineType.Output, value: 'Opening Projects page...'});
-            window.location.href = process.env.PUBLIC_URL + '/projects/';
-        } else if (string.toLocaleLowerCase() === 'rootme') {
-            ld.push({type: LineType.Output, value: 'Opening Root-Me profile...'});
-            window.open('https://www.root-me.org/FreezingKas', '_blank');
-        } else if (string.toLocaleLowerCase() === 'hackthebox') {
-            ld.push({type: LineType.Output, value: 'Opening HackTheBox profile...'});
-            window.open('https://app.hackthebox.com/users/472492', '_blank');
-        } else if (string.toLocaleLowerCase() === 'tryhackme') {
-            ld.push({type: LineType.Output, value: 'Opening TryHackMe profile...'});
-            window.open('https://tryhackme.com/p/FreezingKas', '_blank');
-        } else if (string.toLocaleLowerCase() === 'clear') {
-            ld = []
-        } else {
-            ld.push({type: LineType.Output, value: 'Unknown command'});
+
+        switch(string.toLocaleLowerCase()) {
+            case 'help':
+                const line = ['Available commands :', 
+                        'help : Displays this help message', 
+                        'whoami : Displays who is FreezingKas', 
+                        'github : Displays my GitHub profile', 
+                        'write-ups : Open my write-ups page', 
+                        'projects : Open my projects page', 
+                        'rootme : Open my root-me profile', 
+                        'hackthebox : Open my HackTheBox profile', 
+                        'tryhackme : Open my TryHackMe profile', 
+                        'clear : Clears the terminal'];
+
+                for(let i = 0; i < line.length; i++) {
+                    ld.push({type: LineType.Output, value: line[i]});
+                }
+                break;
+            case 'whoami':
+                ld.push({type: LineType.Output, value: 'FreezingKas : Software Cyber Security student at ENSIBS (Vannes)'});
+                break;
+            case 'github':
+                ld.push({type: LineType.Output, value: 'Opening Github profile...'});
+                window.open('https://github.com/FreezingKas', '_blank');
+                break;
+            case 'write-ups':
+                ld.push({type: LineType.Output, value: 'Opening Write-Ups page...'});
+                window.open(process.env.URL + 'write-ups/', '_blank');
+                break;
+            case 'projects':
+                ld.push({type: LineType.Output, value: 'Opening Projects page...'});
+                window.location.href = process.env.PUBLIC_URL + '/projects/';
+                break;
+            case 'rootme':
+                ld.push({type: LineType.Output, value: 'Opening Root-Me profile...'});
+                window.open('https://www.root-me.org/FreezingKas', '_blank');
+                break;
+            case 'hackthebox':
+                ld.push({type: LineType.Output, value: 'Opening HackTheBox profile...'});
+                window.open('https://app.hackthebox.com/users/472492', '_blank');
+                break;
+            case 'tryhackme':
+                ld.push({type: LineType.Output, value: 'Opening TryHackMe profile...'});
+                window.open('https://tryhackme.com/p/FreezingKas', '_blank');
+                break;
+            case 'clear':
+                ld = []
+                break;
+            default:
+                ld.push({type: LineType.Output, value: 'Unknown command'});
+                break;
         }
         setTerminalLineData(ld);
     }
+
+    const header = (window.innerWidth < 768 ? '~ ' : 'max@freezingkas: ~ ')
 
     return (
         <>
@@ -63,10 +82,10 @@ function App() {
                     <p className='pipe'>|</p>
                     <Link className='link' to="/projects">Projects</Link>
                     <p className='pipe'>|</p>
-                    <Link className='link' to="">CTF Team</Link>
+                    <Link className='link' to="/ctfteam">CTF Team</Link>
                 </nav>
                 <div className='terminal'>
-                    <Terminal name='max@freezingkas: ~'
+                    <Terminal name={header}
                         colorMode={ColorMode.Dark}
                         lineData={terminalLineData}
                         onInput={onInput}
--- front/src/index.js
+++ front/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import WriteUps from './components/WriteUps'
 import Projects from './components/Projects'
+import CTFTeam from './components/CTFTeam';
 import reportWebVitals from './reportWebVitals';
 import {
   BrowserRouter,
@@ -18,6 +19,7 @@ ReactDOM.render(
       <Route path="/" element={<App />} />
       <Route path="write-ups" element={<WriteUps/>} />
       <Route path="projects" element={<Projects/>} />
+      <Route path="ctfteam" element={<CTFTeam/>} />
     </Routes>
   </BrowserRouter>
   </React.StrictMode>,
