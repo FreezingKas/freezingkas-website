ab0418f535915e744ed3a1ca56af0d8b414dcf02
FreezingKas <57356272+FreezingKas@users.noreply.github.com>
2023-02-10 23:05:09 +0100

    Meta

--- components/MyHead.tsx
+++ components/MyHead.tsx
@@ -13,7 +13,7 @@ export default function MyHead(props: MyHeadProps) {
             <meta property="og:description" content="MAK'HACK Member"></meta>
             <meta property="og:image" content="/hero-back.jpg"></meta>
             <meta property="og:url" content="/"></meta>
-            <meta name="twitter:card" content="/hero-back.jpg"/>
+            <meta name="twitter:card" content="summary_large_image"/>
             <link rel="stylesheet" href="/main.css"/>
         </Head>
     )
