b648e34eeb60c2c6b458172bbd4f03ecef447ec5
FreezingKas <57356272+FreezingKas@users.noreply.github.com>
2023-04-02 22:00:56 +0200

    Add blockchain

--- routes/projects/index.tsx
+++ routes/projects/index.tsx
@@ -17,13 +17,13 @@ export default function Projects() {
                         Study Go Ransomware
                         <i><span class="text-xs text-gray-500">Personal</span></i>
                     </a>
-                    <a href="https://github.com/M-k-Hack/Kubi"
+                    <a href="https://github.com/M-k-Hack/not-a-seedbox"
                        class="text-black text-center font-bold w-64 h-64 flex flex-col items-center justify-center bg-[#FFFAFA] shadow-xl text-2xl p-4 text-center rounded-xl
                     transform transition duration-500 hover:scale-110 hover:shadow-2xl m-4">
                         Not A Seedbox
                         <i><span class="text-xs text-gray-500">Team</span></i>
                     </a>
-                    <a href="https://github.com/M-k-Hack/not-a-seedbox"
+                    <a href="https://github.com/M-k-Hack/Kubi"
                        class="text-black text-center font-bold w-64 h-64 flex flex-col items-center justify-center bg-[#FFFAFA] shadow-xl text-2xl p-4 text-center rounded-xl
                     transform transition duration-500 hover:scale-110 hover:shadow-2xl m-4">
                         Kubi
@@ -34,6 +34,12 @@ export default function Projects() {
                     transform transition duration-500 hover:scale-110 hover:shadow-2xl m-4">
                         CTF de l'Avent
                         <i><span class="text-xs text-gray-500">Team</span></i>
+                    </a>
+                    <a href="https://github.com/GCC-ENSIBS/blockchain-gcc"
+                       className="text-black text-center font-bold w-64 h-64 flex flex-col items-center justify-center bg-[#FFFAFA] shadow-xl text-2xl p-4 text-center rounded-xl
+                    transform transition duration-500 hover:scale-110 hover:shadow-2xl m-4">
+                        GCC Private Blockchain
+                        <i><span className="text-xs text-gray-500">School</span></i>
                     </a>
                 </div>
             </div>
